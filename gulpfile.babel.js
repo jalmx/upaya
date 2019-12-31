9538a8c836abd1cafbace0dbafb054706f5104a9
Josef Leyva <jalm_x@hotmail.com>
2019-12-31 17:09:34 -0600

    todo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,6 @@ import browserSync from 'browser-sync'
 import sass from 'gulp-sass'
 import postcss from 'gulp-postcss'
 import cssnano from 'cssnano'
-// import watch from 'gulp-watch'
 import browserify from 'browserify'
 import babelify from 'babelify'
 import source from 'vinyl-source-stream'
@@ -15,6 +14,9 @@ import minify from 'gulp-minify'
 import imagemin from 'gulp-imagemin'
 import sitemap from 'gulp-sitemap'
 import cachebust from 'gulp-cache-bust'
+import htmlmin from 'gulp-htmlmin';
+import humans from 'gulp-humans';
+import robots from 'gulp-robots';
 
 const server = browserSync.create()
 
@@ -32,7 +34,7 @@ const postcssPlugins = [
 const sitemapTask = () => {
     return src('./public/**/*.html', { read: false })
         .pipe(sitemap({
-            siteUrl: "https://www.upayatech.com"
+            siteUrl: "https://www.upayatech.work"
         }))
         .pipe(dest("./public"))
 
@@ -50,10 +52,10 @@ const pugTask = () => {
 const sassTask = () => {
     return src('./src/sass/style.scss')
         .pipe(sass()).on('error', (e) => { console.log('error en la compilacion SASS', e) })
-        .pipe(plumber())
+        // .pipe(plumber())
         .pipe(postcss(postcssPlugins)).on('error', (e) => { console.log('error en la compilacion POST CSS'), e })
         .pipe(dest('./public/css'))
-        // .pipe(server.stream({ match: '**/*.css' }))
+        .pipe(server.stream())
 }
 
 const imgTask = () => {
@@ -65,39 +67,89 @@ const imgTask = () => {
             imagemin.svgo()
         ]))
         .pipe(dest('./public/assets/img'))
-        
 }
 
 const initServer = () => {
     server.init({
         server: {
-          baseDir: './public'
+            baseDir: './public'
         }
-      })
+    })
+    console.log('inicia el servidor');
+
 }
 
-const fontTask= () =>{
-return src("src/assets/fonts/**")
-    .pipe(dest("public/assets/fonts"))
+const fontTask = () => {
+    return src("src/assets/fonts/**")
+        .pipe(dest("public/assets/fonts"))
+}
+
+const humanTask = () => {
+    return src('public/index.html')
+        .pipe(humans({
+            team: [{
+                "Original developer": "Alejandro Leyva",
+                Twitter: "@jalm_x",
+                Github: "@jalmx"
+            },
+            {
+                Maintainer: "Xizuth Inc",
+                Github: "@xizuth_tech"
+            }],
+            thanks: [
+                'Node (@nodejs on Twitter)',
+                'Gulp (@gulpjs on Twitter)'
+            ],
+            site: [
+                'Standards: HTML5, CSS, ES6',
+                'Components: Pug, SASS, Google Fonts, Material Design Icons',
+                'Software: Visual Studio Code'
+            ],
+            note: 'Built with love by Alejandro Leyva.'
+        }))
+        .pipe(dest('public/'));
+}
+
+const robotTask = () => {
+    return src('./public/index.html')
+        .pipe(robots({
+            useragent: '*',
+            allow: ['/'],
+            disallow: ['cgi-bin/']
+        }))
+        .pipe(dest('public/'));
+}
+
+const cacheTask = () => {
+    return src('./public/**/**.html')
+        .pipe(cachebust({
+            type: 'timestamp'
+        }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('./public'))
 }
 
 exports.fonts = fontTask;
+exports.humans = humanTask;
+exports.robot = robotTask;
+exports.cache = cacheTask;
 exports.pug = pugTask;
+exports.sitemap = sitemapTask;
 exports.imgs = imgTask;
 exports.sass = sassTask;
-exports.build = series(pugTask, imgTask, sassTask, fontTask), initServer;
+exports.build = series(pugTask, imgTask, sassTask, fontTask, humanTask, robotTask, cacheTask, sitemapTask, initServer);
 exports.default = series(pugTask, sassTask, initServer);
 
 const watcher = watch(["./src"])
 
-watcher.on('all', (path, stats)=>{
-    console.log(`File ${path} was changed`);  
-    console.log(`File ${stats} estado`);  
-    pugTask()
-    sassTask()
-    server.reload()
+watcher.on('all', (path, stats) => {
+    
+    if (stats.indexOf('sass') > 0) {
+        sassTask()
+        console.log('actualiza css');
+    } else if (stats.indexOf('pug') > 0) {
+        pugTask()
+        console.log('actualiza html');
+        server.reload()
+    }
 })
-
-// watcher.close()
-
-//TODO HUMAN, robots, cache 
